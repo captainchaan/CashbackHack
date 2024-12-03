653bddc434ab44326e362225ac43bd7fc9cf6eae
TEST <annivanova@list.ru>
2024-12-03 20:08:50 +0300

    add Testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -17,4 +17,16 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+    @Test
+    public void needToBuy1(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+
+        assertEquals(expected, actual);
+    }
+
 }
